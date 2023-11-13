d73e083d0528d4ce1784771a8cba9dc561ab723f
Azhar Amir Kimanje <77221185+kimanjea@users.noreply.github.com>
2023-11-13 11:32:51 -0800

    3D XR Cube physics

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float horizontalAxis;
+    public float verticalAxis;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+
+        transform.Rotate(10.0f * Time.deltaTime, 1.0f, 5.0f);
+      //  transform.position = new Vector3(0, 0, 0);
+
+        transform.Translate(Vector3.up * Time.deltaTime* verticalAxis * 10.0f);
+        transform.Translate(Vector3.left * Time.deltaTime* horizontalAxis * 10.0f);
+
+        
+    }
+}
